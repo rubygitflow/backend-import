9507af2b3bcd8bf29f5bfcc6a29b958c7e5324a3
it.Architect <nrubygitflow@gmail.com>
2021-05-06 01:09:28 +0300

    streaming import

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,8 @@ class ProductsController < ApplicationController
       Product.import(params[:file])
       redirect_to products_path, notice: "Products added successfully"
     rescue CSV::MalformedCSVError => e
+      redirect_to products_path, notice: "Error in the input data structure: #{e.message}"
+    rescue Exception => e
       redirect_to products_path, notice: "Something went wrong: #{e.message}"
     end
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,42 +1,56 @@
 class Product < ApplicationRecord
 
 	def self.import(file)
-    price_list = file.original_filename.to_s
-    Product.where(price_list: price_list).destroy_all
+    mem_usage_before = print_memory if Rails.env.development?
+    time = Benchmark.realtime do
+      price_list = file.original_filename.to_s
+      ActiveRecord::Base.transaction do
+        products_command =
+          "copy products (price_list, brand, code, stock, cost, name, created_at, updated_at) from stdin with csv delimiter ';'"
 
-    strVar = File.open(file.path, &:readline)
-    col_del = strVar.index(';').nil? ? ',' : ';'
+        Product.where(price_list: price_list).delete_all
+        ActiveRecord::Base.connection.reset_pk_sequence!('products')
 
-    begin
-      CSV.foreach(file.path, 
-                  headers: :first_row, 
-                  liberal_parsing: true, 
-                  col_sep: col_del
-                 ) do |row|
-        begin
-          input_hash = row.to_hash
-          hsh = select_hash(input_hash, price_list)
-          Product.create! hsh if !hsh['brand'].empty? and !hsh['code'].empty? 
-        rescue 
-          # ignore record
-        end
+        strVar = File.open(file.path, &:readline)
+        col_del = strVar.index(';').nil? ? ',' : ';'
 
-      end
-    rescue CSV::MalformedCSVError
-      CSV.foreach(file.path, 
-                  headers: :first_row, 
-                  liberal_parsing: true, 
-                  encoding: 'CP1251:utf-8', 
-                  col_sep: col_del
-                 ) do |row|
-        begin
-          input_hash = row.to_hash
-          hsh = select_hash(input_hash, price_list)
-          Product.create! hsh if !hsh['brand'].nil? and !hsh['code'].nil? 
-        rescue 
-          # ignore record
-        end
-      end
+        raw_connection.copy_data products_command do
+          begin
+            # https://ruby-doc.org/stdlib-2.7.2/libdoc/csv/rdoc/CSV.html
+            CSV.foreach(file.path, 
+                        headers: :first_row, 
+                        liberal_parsing: true, 
+                        col_sep: col_del
+                       ) do |row|
+              begin
+                import_action(row.to_hash, price_list)
+              rescue 
+                # ignore record
+              end
+
+            end
+          rescue CSV::MalformedCSVError
+            CSV.foreach(file.path, 
+                        headers: :first_row, 
+                        liberal_parsing: true, 
+                        encoding: 'CP1251:utf-8', 
+                        col_sep: col_del
+                       ) do |row|
+              begin
+                import_action(row.to_hash, price_list)
+              rescue 
+                # ignore record
+              end
+            end
+          end
+        end 
+      end 
+    end 
+    if Rails.env.development?
+      mem_usage_after = print_memory
+      mem_usage = mem_usage_after - mem_usage_before
+      puts "Memory usage: #{mem_usage} KB"
+      puts "Finish in: #{time.round(2)}"
     end
   end	
 
@@ -47,15 +61,41 @@ class Product < ApplicationRecord
 
   private
 
-  def self.select_hash(input_hash, file_name)
-    res = {}
-    res['price_list'] = file_name
-    res['brand'] = input_hash['Производитель'] || input_hash['Бренд']
-    res['code'] = input_hash['Артикул'] || input_hash["﻿\"Артикул\""] || input_hash['Номер']
-    res['stock'] = input_hash['Количество'] || input_hash['Кол-во']
-    res['stock'] = res['stock'].index('>').nil? ? res['stock'] : res['stock'].gsub('>', '')
-    res['cost'] = input_hash['Цена']
-    res['name'] = input_hash['Наименование'] || input_hash['НаименованиеТовара']
-    res
+  def self.raw_connection
+    @raw_connection ||= ActiveRecord::Base.connection.raw_connection
   end
+
+  def self.import_action(input_hash, file_name)
+    hsh = {}
+    hsh['brand'] = input_hash['Производитель'] || input_hash['Бренд']
+    hsh['code'] = input_hash['Артикул'] || input_hash["﻿\"Артикул\""] || input_hash['Номер']
+
+    if !hsh['brand'].empty? && !hsh['code'].empty? 
+      hsh['price_list'] = file_name
+      hsh['stock'] = input_hash['Количество'] || input_hash['Кол-во']
+      hsh['stock'] = hsh['stock'].index('>').nil? ? hsh['stock'] : hsh['stock'].gsub('>', '')
+      hsh['cost'] = input_hash['Цена'].tr(',', '.')
+      hsh['name'] = input_hash['Наименование'] || input_hash['НаименованиеТовара']
+      # puts("#{hsh['name']}")
+      # TODO: How sanitize sql/csv here? Escape_symbol doesn't work. :(
+      hsh['name'].gsub!('"', '') # грубое исправление проблемы
+
+      time = Time.now.localtime.strftime("%Y-%m-%d %H:%M:%S")
+      # стримим подготовленный чанк данных в postgres 
+      s = "#{hsh['price_list']};#{hsh['brand']};#{hsh['code']};#{hsh['stock']};#{hsh['cost']};#{hsh['name']};#{time};#{time}\n"
+      raw_connection.put_copy_data(s)
+    end 
+  end
+
+
+  def self.memory
+    `ps -o rss= -p #{Process.pid}`.to_i
+  end
+
+  def self.print_memory
+    m = memory
+    puts "Memory: #{m} KB"
+    m
+  end
+
 end
