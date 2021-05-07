fdad0d09200ef4d839e281989e962dae948383fe
it.Architect <nrubygitflow@gmail.com>
2021-05-08 00:06:03 +0300

    fixed quotes' problem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,15 @@
 class Product < ApplicationRecord
+  QUOTE_CHAR = '^'.freeze    
+  COLUMNS = "price_list, brand, code, stock, cost, name, created_at, updated_at".freeze
 
 	def self.import(file)
     mem_usage_before = print_memory if Rails.env.development?
     time = Benchmark.realtime do
       price_list = file.original_filename.to_s
       ActiveRecord::Base.transaction do
+        # https://postgrespro.ru/docs/enterprise/12/sql-copy
         products_command =
-          "copy products (price_list, brand, code, stock, cost, name, created_at, updated_at) from stdin with csv delimiter ';'"
+          "copy products (#{COLUMNS}) from stdin with csv delimiter ';' quote '#{QUOTE_CHAR}'"
 
         Product.where(price_list: price_list).delete_all
         ActiveRecord::Base.connection.reset_pk_sequence!('products')
@@ -22,12 +25,11 @@ class Product < ApplicationRecord
                         liberal_parsing: true, 
                         col_sep: col_del
                        ) do |row|
-              begin
+              # begin
                 import_action(row.to_hash, price_list)
-              rescue 
-                # ignore record
-              end
-
+              # rescue 
+              #   # ignore record
+              # end
             end
           rescue CSV::MalformedCSVError
             CSV.foreach(file.path, 
@@ -36,11 +38,11 @@ class Product < ApplicationRecord
                         encoding: 'CP1251:utf-8', 
                         col_sep: col_del
                        ) do |row|
-              begin
+              # begin
                 import_action(row.to_hash, price_list)
-              rescue 
-                # ignore record
-              end
+              # rescue 
+              #   # ignore record
+              # end
             end
           end
         end 
@@ -60,7 +62,6 @@ class Product < ApplicationRecord
   end
 
   private
-
   def self.raw_connection
     @raw_connection ||= ActiveRecord::Base.connection.raw_connection
   end
@@ -68,19 +69,20 @@ class Product < ApplicationRecord
   def self.import_action(input_hash, file_name)
     hsh = {}
     hsh['brand'] = input_hash['Производитель'] || input_hash['Бренд']
-    hsh['code'] = input_hash['Артикул'] || input_hash["﻿\"Артикул\""] || input_hash['Номер']
+    hsh['code'] = input_hash['Артикул'] || 
+                  input_hash["﻿\"Артикул\""] || input_hash['Номер']
 
     if !hsh['brand'].empty? && !hsh['code'].empty? 
       hsh['price_list'] = file_name
       hsh['stock'] = input_hash['Количество'] || input_hash['Кол-во']
-      hsh['stock'] = hsh['stock'].index('>').nil? ? hsh['stock'] : hsh['stock'].gsub('>', '')
+      hsh['stock'] = hsh['stock'].index('>').nil? ? 
+        hsh['stock'] : hsh['stock'].gsub('>', '')
       hsh['cost'] = input_hash['Цена'].tr(',', '.')
-      hsh['name'] = input_hash['Наименование'] || input_hash['НаименованиеТовара']
-      # puts("#{hsh['name']}")
-      # TODO: How sanitize sql/csv here? Escape_symbol doesn't work. :(
-      hsh['name'].gsub!('"', '') # грубое исправление проблемы
+      hsh['name'] = input_hash['Наименование'] || 
+                    input_hash['НаименованиеТовара']
 
       time = Time.now.localtime.strftime("%Y-%m-%d %H:%M:%S")
+
       # стримим подготовленный чанк данных в postgres 
       s = "#{hsh['price_list']};#{hsh['brand']};#{hsh['code']};#{hsh['stock']};#{hsh['cost']};#{hsh['name']};#{time};#{time}\n"
       raw_connection.put_copy_data(s)
